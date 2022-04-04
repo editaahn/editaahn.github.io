5504c1607029fa440545e89c59d0e152006beef8
Rita <41330862+editaahn@users.noreply.github.com>
2022-04-04 23:10:41 +0900

    correct previous and next page button logic (#32)

--- gatsby-node.js
+++ gatsby-node.js
@@ -58,8 +58,8 @@ exports.createPages = async ({ graphql, actions }) => {
   postGroups.forEach(({ edges: posts, fieldValue: category }) => {
     // Create blog posts pages
     posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node;
-      const next = index === 0 ? null : posts[index - 1].node;
+      const next = index === posts.length - 1 ? null : posts[index + 1].node;
+      const previous = index === 0 ? null : posts[index - 1].node;
 
       createPage({
         path: post.node.fields.slug,
